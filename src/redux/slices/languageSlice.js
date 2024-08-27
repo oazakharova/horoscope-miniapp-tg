7a58ff34a55affc1acaa2dfd527fd9dd08bc3165
Olga <o.nikolaichenko@ya.ru>
2024-08-27 12:01:00 +0200

    logToScreen function has been added

--- src/redux/slices/languageSlice.js
+++ src/redux/slices/languageSlice.js
@@ -1,8 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const logToScreen = (message) => {
+  const logElement = document.createElement('div');
+  logElement.style.position = 'absolute';
+  logElement.style.top = '0';
+  logElement.style.left = '0';
+  logElement.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+  logElement.style.color = '#fff';
+  logElement.style.padding = '5px';
+  logElement.style.zIndex = '9999';
+  logElement.innerText = message;
+
+  document.body.appendChild(logElement);
+};
+
 const getDefaultLanguage = () => {
   const language = window.Telegram.WebApp.initDataUnsafe.language;
-  console.log(language);
+  logToScreen(
+    'Telegram WebApp initDataUnsafe: ' +
+      JSON.stringify(window.Telegram.WebApp.initDataUnsafe)
+  );
+  logToScreen(
+    'Telegram WebApp initDataUnsafe language: ' +
+      JSON.stringify(window.Telegram.WebApp.initDataUnsafe.language)
+  );
   return language === 'ru' ? 'ru' : 'en';
 };
 
